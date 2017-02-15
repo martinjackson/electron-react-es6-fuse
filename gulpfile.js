21f54609d7f3e2c73d7849cc77ad3ae7fffbec39
Martin Jackson <martin.a.jackson@gmail.com>
2017-02-15 14:59:24 -0600

    intial gulp sequencing could go awry

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,6 @@
 
 const gulp = require('gulp');
 var rename = require("gulp-rename");
-var runSequence = require('run-sequence');
 var exec = require('child_process').exec;
 
 const electron = require('electron-connect').server.create();
@@ -15,11 +14,10 @@ let frontCode = fuseHelper("src/",  "main/public/bundle.js");
 
 gulp.task("restart",                ()=> { electron.restart() })
 gulp.task("reload",                 ()=> { electron.reload() })
-gulp.task("bundleRend",             ()=> { frontCode.bundle('>renderer.js') })
-gulp.task("prep", ()=> { runSequence('bundle','copy', 'loadModules'); })
-gulp.task("bundle", ['bundleRend']);
 
-gulp.task("copy", ()=> {
+gulp.task("prep", ()=> {
+    frontCode.bundle('>renderer.js')
+
     gulp.src('main/public/**/*.*', {base: 'main/public/'})
       .pipe(gulp.dest('./dist/public'))
 
@@ -33,21 +31,9 @@ gulp.task("copy", ()=> {
               console.log(stderr);
               cb(err);
             });
-});
 
-gulp.task('loadModules', (cb)=> {
   process.chdir('dist');
   exec('yarn install',
-      (err, stdout, stderr)=> {
-          console.log(stdout);
-          console.log(stderr);
-          cb(err);
-        });
-})
-
-gulp.task('test', (cb)=> {
-  process.chdir('dist');
-  exec('pwd',
       (err, stdout, stderr)=> {
           console.log(stdout);
           console.log(stderr);
