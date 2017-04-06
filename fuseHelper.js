a0b9a5c63cb6ff20f25855f2ee498e0fcfd3ab5a
Martin Jackson <martin.a.jackson@gmail.com>
2017-04-06 09:30:37 -0500

    fuseHelper.js now uses babel config from package.json

--- fuseHelper.js
+++ fuseHelper.js
@@ -1,10 +1,23 @@
 
 const fsbx = require("fuse-box");
 
+let pkgconfig = require('./package.json');
+
 var fuseHelper = function(homeDir, outFile) {
 
   const path = outFile.substring(0,outFile.lastIndexOf('/')+1);
   const bundle = outFile.substring(outFile.lastIndexOf('/')+1)+'.map';
+
+//           fsbx.BabelPlugin({ config: { sourceMaps: true, presets: ["latest", "react", "stage-2"] } })
+
+  if (pkgconfig.babel === undefined) {
+    console.log('package.json needs a babel configuration section, like the following: ');
+    console.log('   "babel": {                                    ');
+    console.log('     "sourceMaps": true,                         ');
+    console.log('     "presets": [ "react", "latest", "stage-2" ] ');
+    console.log('   },                                            ');
+  }
+
 
   const config = {
       serverBundle: true,
@@ -16,7 +29,7 @@ var fuseHelper = function(homeDir, outFile) {
           fsbx.JSONPlugin(),
           fsbx.SVGPlugin(),
           fsbx.CSSPlugin({ group: "bundle.css", outFile: `${path}/bundle.css` }),
-          fsbx.BabelPlugin({ config: { sourceMaps: true, presets: ["latest", "react", "stage-2"] } })
+          fsbx.BabelPlugin({ config: pkgconfig.babel })
       ],
 
       homeDir: homeDir,
